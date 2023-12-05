c67f2e5e5e6137007c496332f1640861e783d28b
Alessandra Cappati <alessandra.cappati@cern.ch>
2023-12-05 18:25:50 +0100

    add 2D plot eta vs en

--- plotter_pho-pi_samples.py
+++ plotter_pho-pi_samples.py
@@ -1009,7 +1009,47 @@ def doMultiplicityPlots_cat(data_list_pho: List[Data], data_list_pi: List[Data],
     plt.close(fig)
 
     
+# function to plot visualization plots
+def doVisualizationPlots(data_list_pho: List[Data], data_list_pi: List[Data], out_dir: str) -> None:
 
+    # PIONS
+    shower_en_pi = []
+    shower_eta_pi = []
+    # read 3D objects
+    # trkcluseta,trkclusphi,trkclusen,trkclustime, min(clusL),max(clusL)
+    for i_file_pi in data_list_pi:
+        for i_evt_pi in i_file_pi:
+            trackster_pi = i_evt_pi.clus3d_feat.numpy()
+    
+            shower_en_pi.append(trackster_pi[2])
+            shower_eta_pi.append(trackster_pi[0])
+
+    # PHOTONS
+    shower_en_pho = []
+    shower_eta_pho = []
+    # read 3D objects
+    # trkcluseta,trkclusphi,trkclusen,trkclustime, min(clusL),max(clusL)
+    for i_file_pho in data_list_pho:
+        for i_evt_pho in i_file_pho:
+            trackster_pho = i_evt_pho.clus3d_feat.numpy()
+    
+            shower_en_pho.append(trackster_pho[2])
+            shower_eta_pho.append(trackster_pho[0])
+    
+    # plot eta vs energy 
+    fig, axs = plt.subplots(1, 2, figsize=(20,10), tight_layout=True)  
+    axs[0].hist2d(shower_en_pho, shower_eta_pho, bins=50, range=((0,1200),(1.2,3.2)), density=True, cmap='Oranges')
+    axs[0].set_xlabel('Energy')
+    axs[0].set_ylabel('eta')
+    axs[0].set_title(r'$\gamma$')  
+    axs[1].hist2d(shower_en_pi, shower_eta_pi, bins=50, range=((0,1200),(1.2,3.2)), density=True, cmap='Greens')
+    axs[1].set_xlabel('Energy')
+    axs[1].set_ylabel('eta')
+    axs[1].set_title(r'$\pi$')
+    plt.savefig(os.path.join(out_dir, 'eta_en_trackster.png')) #save plot
+    plt.close(fig)
+
+    
 
 if __name__ == "__main__" :
 
@@ -1030,7 +1070,7 @@ if __name__ == "__main__" :
     ## plots
     print('doing plots...')
     
-    doHisto(data_list_pho, data_list_pi, out_dir)
+    #doHisto(data_list_pho, data_list_pi, out_dir)
 
     #doENprofile(data_list_pho, data_list_pi, out_dir)
 
@@ -1039,3 +1079,5 @@ if __name__ == "__main__" :
     #doMultiplicityPlots(data_list_pho, data_list_pi, out_dir)
 
     #doMultiplicityPlots_cat(data_list_pho, data_list_pi, out_dir)
+
+    doVisualizationPlots(data_list_pho, data_list_pi, out_dir)
